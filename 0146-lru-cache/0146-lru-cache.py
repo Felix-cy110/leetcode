e4028d2ca6065dad53eafc54d67031fa89886048
Felix Sun <3418786206@qq.com>
2026-09-23 00:40:56 +0800

    Time: 31 ms (70.44%), Space: 29.2 MB (57.06%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -2,24 +2,24 @@ class DLinkedNode:
     def __init__(self, key=0, value=0):
         self.key = key
         self.value = value
-        self.prev = None
         self.next = None
-
+        self.prev = None
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
+        self.size = 0
         self.cache = dict()
         self.head = DLinkedNode()
         self.tail = DLinkedNode()
         self.head.next = self.tail
         self.tail.prev = self.head
-        self.size = 0
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-        self.moveToHead(self.cache[key])
-        return self.cache[key].value
+        node = self.cache[key]
+        self.moveToHead(node)
+        return node.value
     def put(self, key: int, value: int) -> None:
         if key not in self.cache:
             node = DLinkedNode(key, value)
@@ -27,32 +27,32 @@ class LRUCache:
             self.addToHead(node)
             self.size += 1
             if self.size > self.capacity:
-                removed = self.removeTail()
-                self.cache.pop(removed.key)
+                removedNode = self.removeTail()
                 self.size -= 1
+                self.cache.pop(removedNode.key)
         else:
             node = self.cache[key]
             node.value = value
             self.moveToHead(node)
+
     def addToHead(self, node):
         self.head.next.prev = node
         node.next = self.head.next
-        self.head.next = node
         node.prev = self.head
-    
-    def removeNode(self, node):
-        node.prev.next = node.next
+        self.head.next = node
+
+    def remove(self, node):
         node.next.prev = node.prev
-    
+        node.prev.next = node.next
+
     def moveToHead(self, node):
-        self.removeNode(node)
+        self.remove(node)
         self.addToHead(node)
 
     def removeTail(self):
         node = self.tail.prev
-        self.removeNode(node)
+        self.remove(node)
         return node
-    
         
 
 
