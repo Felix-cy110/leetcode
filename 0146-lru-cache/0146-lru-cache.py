f410d7b5c5bffa8c2ba8d4f39006bbe9a7ca60b5
Felix Sun <3418786206@qq.com>
2026-09-26 21:17:06 +0800

    Time: 27 ms (75.62%), Space: 29.1 MB (55.23%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,5 +1,5 @@
 class DLinkedNode:
-    def __init__(self, key=0,value=0):
+    def __init__(self, key=0, value=0):
         self.key = key
         self.value = value
         self.next = None
@@ -7,34 +7,35 @@ class DLinkedNode:
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.size = 0
         self.capacity = capacity
+        self.size = 0
         self.cache = dict()
         self.head = DLinkedNode()
         self.tail = DLinkedNode()
         self.head.next = self.tail
         self.tail.prev = self.head
+
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
         node = self.cache[key]
         self.moveToHead(node)
         return node.value
+
     def put(self, key: int, value: int) -> None:
         if key not in self.cache:
             node = DLinkedNode(key, value)
+            self.cache[key] = node
             self.addToHead(node)
             self.size += 1
-            self.cache[key] = node
             if self.size > self.capacity:
                 removed = self.removeTail()
-                self.size -= 1
                 self.cache.pop(removed.key)
+                self.size -= 1
         else:
             node = self.cache[key]
             node.value = value
             self.moveToHead(node)
-
     def moveToHead(self, node):
         self.remove(node)
         self.addToHead(node)
@@ -42,8 +43,8 @@ class LRUCache:
     def remove(self, node):
         node.next.prev = node.prev
         node.prev.next = node.next
-
-    def addToHead(self,node):
+    
+    def addToHead(self, node):
         node.next = self.head.next
         node.prev = self.head
         self.head.next.prev = node
@@ -53,7 +54,6 @@ class LRUCache:
         node = self.tail.prev
         self.remove(node)
         return node
-        
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
